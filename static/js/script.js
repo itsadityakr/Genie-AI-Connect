75e7cea2a636bade32642ad1d309d98c339efa00
Aditya Kumar <itsadityakr27@gmail.com>
2023-10-23 22:29:02 +0530

    Modified
    found error in darkModeToggle.css

--- static/js/script.js
+++ static/js/script.js
@@ -49,3 +49,9 @@ buttonElement.addEventListener("click", function() {
 function reloadPage() {
   location.reload();
 }
+var content = document.getElementsByTagName('body')[0];
+        var darkMode = document.getElementById('dark-change');
+        darkMode.addEventListener('click', function(){
+            darkMode.classList.toggle('active');
+            content.classList.toggle('night');
+        })
